2f1e53a4996491e77f13c3247bffd3506303aba6
senreot <senreot>
2016-10-06 14:31:42 +0200

    Create an empty project

---
+++ ClimateControlAplication/main.c
@@ -0,0 +1,18 @@
+/*
+ * ClimateControlAplication.c
+ *
+ * Created: 06/10/2016 14:28:27
+ * Author : senreot
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
